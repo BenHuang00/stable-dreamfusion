1c88a5334a4f0e9dc9210e91d17bb2232a855636
BugMakerH <1371444344@qq.com>
2025-03-26 15:56:39 +0800

    fix(sd_utils.py): adjust depthfm_ratio calculation to normalize by max_step
    Normalize the depthfm_ratio by dividing t by max_step to ensure the ratio
    scales correctly across different training steps. This change aims to
    improve the stability and accuracy of the depth loss contribution during
    training.

--- guidance/sd_utils.py
+++ guidance/sd_utils.py
@@ -224,7 +224,7 @@ class StableDiffusion(nn.Module):
 
         if self.depthfm_model is not None:
             depth_loss = self.get_depthfm_loss(pred_x0, depth, latents_noisy, save_guidance_path=save_guidance_path)
-            ratio = (self.depthfm_ratio * (1 - t))
+            ratio = (self.depthfm_ratio * (1 - t / self.max_step))
             wandb.log({"train_depthfm_ratio": ratio.item()})        # TODO(Ben): Need to delete
             loss += ratio * depth_loss
             if wandb.run is not None:
