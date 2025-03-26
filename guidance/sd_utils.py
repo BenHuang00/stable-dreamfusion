e346190bd85a1f403d10fb78544352ba0edbb6a1
BugMakerH <1371444344@qq.com>
2025-03-26 16:39:12 +0800

    feat(sd_utils.py): add logging for depthfm ratio to aid in debugging and monitoring of model training process

--- guidance/sd_utils.py
+++ guidance/sd_utils.py
@@ -225,7 +225,8 @@ class StableDiffusion(nn.Module):
         if self.depthfm_model is not None:
             depth_loss = self.get_depthfm_loss(pred_x0, depth, latents_noisy, save_guidance_path=save_guidance_path)
             ratio = torch.cat([(self.depthfm_ratio * (1 - t / self.max_step))])
-            
+            print(f'[INFO] depthfm ratio: {ratio}')
+            print(f'[INFO] depthfm ratio: {ratio.item()}')
             loss += ratio * depth_loss
             if wandb.run is not None:
                 wandb.log({"train_depthfm_loss": depth_loss.item()})
